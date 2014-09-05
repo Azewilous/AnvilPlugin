221ecdc59627492a4928bec31cbab45b2d854430
Brandon <OhCreativeYT@gmail.com>
2014-09-05 10:49:47 -0400

    Added API-Type Methods

--- src/main/java/net/shadowcode/ohcreative/LicenseHandler.java
+++ src/main/java/net/shadowcode/ohcreative/LicenseHandler.java
@@ -1,7 +1,51 @@
 public class LicenseHandler {
-
-  public LicenseHandler() {
   
+  private String version, name;
+  private Directory directory;
+  private ArrayList license;
+  private boolean tampered;
+  
+  public LicenseHandler() {
+    this.tampered = null;
+    this.version = "";
+    this.name = "";
+  }
+  
+  public boolean parseLicense() {
+  
+  }
+  
+  public String getLicense() {
+    return license;
+  }
+  
+  public String getLicenseName() {
+    return name;
+  }
+  
+  public String getLicenseVersion() {
+    return version;
+  }
+  
+  public boolean isTampered() {
+    return (tamperTest() != null);
+  }
+  
+  public Directory getFileLocation() {
+    return findFile();
+  }
+
+
+
+  
+
+  protected boolean tamperTest() {
+    
+  }
+  
+  protected Directory findFile() {
+    
+    return this.directory;
   }
   
 }
