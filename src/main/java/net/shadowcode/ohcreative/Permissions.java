5df8604a43aa68e6e2751309f2b57a0845615396
Brandon <nexcre8og@gmail.com>
2014-09-11 12:23:31 -0400

    Update Permissions.java

--- src/main/java/net/shadowcode/ohcreative/Permissions.java
+++ src/main/java/net/shadowcode/ohcreative/Permissions.java
@@ -4,6 +4,6 @@ package net.shadowcode.ohcreative;
 public enum Permissions {
 
  PERMISSION("anvil.anvil"), VERSION("anvil.version"), NOTIFY("anvil.reload.notify"), 
-           RELOAD("anvil.reload"), HELP("anvil.help"), MONEY("anvil.money");
+           RELOAD("anvil.reload"), HELP("anvil.help"), MONEY("anvil.money.exempt");
 
 }
