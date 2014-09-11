23b6b3a9e915028df0222c98a518ab59d5a227ef
Brandon <nexcre8og@gmail.com>
2014-09-11 11:34:02 -0400

    Added Economy (Removed from Economy Manager)

--- src/main/java/net/shadowcode/ohcreative/VaultIntegration.java
+++ src/main/java/net/shadowcode/ohcreative/VaultIntegration.java
@@ -1,6 +1,22 @@
-
+package net.shadowcode.ohcreative;
 
 public final class VaultIntegration {
+
+
+    public static Economy econ = null;
+
+    public static boolean setupEconomy() {
+        Plugin p = Anvil.getPlugin(Anvil.class);
+        if(p.getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = p.getServer().getServicesManager().getRegistration(Economy.class);
+        if(rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
 
 
 
