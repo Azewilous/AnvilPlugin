b2649583515fbda5906051d204a70a839af944bd
Brandon <nexcre8og@gmail.com>
2014-09-11 11:49:37 -0400

    Updated to BETA 1.0 for Bukkit 1.7.10-R0.1

---
+++ src/main/java/net/shadowcode/ohcreative/Permissions.java
@@ -0,0 +1,9 @@
+package net.shadowcode.ohcreative;
+
+
+public enum Permissions {
+
+ PERMISSION("anvil.anvil"), VERSION("anvil.version"), NOTIFY("anvil.notify"), 
+           RELOAD("anvil.reload"), HELP("anvil.help"), MONEY("anvil.money");
+
+}
